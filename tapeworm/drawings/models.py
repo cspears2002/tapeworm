24714a0e3820805d42a90f698cfaa9a2cce10e14
cspears2002 <cspears2002@yahoo.com>
2014-03-26 19:24:32 -0700

    Installed jsonfield.  Started to create the Drawing class.

--- tapeworm/drawings/models.py
+++ tapeworm/drawings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import User
+
+from jsonfield import JSONfield
 
 # Create your models here.
+
+class Drawing(models.Model):
+  owner = models.ForeignKey(User)
+  name = models.CharField()
+  json = JSONfield()

e7ed177f515d4fd4111f781e085c542d33f12be8
cspears2002 <cspears2002@yahoo.com>
2014-03-26 19:51:08 -0700

    Made more changes to models and successfully synced db.

--- tapeworm/drawings/models.py
+++ tapeworm/drawings/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
-from jsonfield import JSONfield
+from jsonfield import JSONField
 
 # Create your models here.
 
 class Drawing(models.Model):
   owner = models.ForeignKey(User)
-  name = models.CharField()
-  json = JSONfield()
+  title = models.CharField(max_length=200)
+  json = JSONField()
+
+  def __unicode__(self):
+    return self.title

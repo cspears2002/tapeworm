c16ad97d7b3c991c34e05e08fc7ab3eca5221434
cspears2002 <cspears2002@yahoo.com>
2014-03-28 11:43:23 -0700

    Setting up app for deployment to Heroku.

--- tapeworm/tapeworm/settings.py
+++ tapeworm/tapeworm/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -80,11 +82,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Templates
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
--- tapeworm/tapeworm/wsgi.py
+++ tapeworm/tapeworm/wsgi.py
@@ -12,3 +12,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tapeworm.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Configuring for Heroku
+# Serve static files in production
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
